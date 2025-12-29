21b0b20d6ebb5d505099d36626e77663fe2692ae
CodeWithBry <bryanagustinpajarillaga@gmaill.com>
2025-12-29 19:08:38 +0800

    first commit

--- src/hooks/useLocaleStorage.ts
+++ src/hooks/useLocaleStorage.ts
@@ -1,35 +1,46 @@
 export function useLocaleStorage(): UseLocaleStorage {
     function saveDataToLocalStorage({ updatedTaskClass, taskType, taskClass }: SaveDataToLocalStorage) {
         const getStorage = localStorage.getItem(taskType);
-    
+
         if (getStorage && updatedTaskClass) {
             const convertData: TaskClass[] = JSON.parse(getStorage);
-            const setUpdatedProject: TaskClass[] = convertData.map(t => {
-                if (t.id == updatedTaskClass.id) {
-                    return { ...updatedTaskClass }
-                }
-                return t
-            }).filter(t => t.taskType == taskType);
-            console.log(setUpdatedProject)
-            localStorage.setItem(taskType, JSON.stringify(setUpdatedProject));
+            const notBelongToTaskClass = taskClass?.filter(t => {
+                if (t.taskType == "normal-tasks") return !convertData.some(ta => t.id == ta.id)
+            })
+
+            if (notBelongToTaskClass) {
+                const updatedTaskClass = [...convertData, ...notBelongToTaskClass];
+                return localStorage.setItem(taskType, JSON.stringify(updatedTaskClass));
+            } else {
+                const setUpdatedProject: TaskClass[] = convertData.map(t => {
+                    if (t.id == updatedTaskClass.id) {
+                        return { ...updatedTaskClass }
+                    }
+                    return t
+                }).filter(t => t.taskType == taskType);
+                console.log(setUpdatedProject)
+                localStorage.setItem(taskType, JSON.stringify(setUpdatedProject));
+            }
+
+
         } else {
             const getSpecificTaskClass = taskClass.filter(t => t.taskType == taskType)
             localStorage.setItem(taskType, JSON.stringify(getSpecificTaskClass));
         }
     }
-    
+
     function getDataFromLocalStorage(): GetDataFromLocalStorage {
         const getNormalTasks = localStorage.getItem("normal-tasks");
         const getProjects = localStorage.getItem("projects");
         const parseNormalTasksData = getNormalTasks ? JSON.parse(getNormalTasks) as TaskClass[] : []
         const parseProjectsData = getProjects ? JSON.parse(getProjects) as TaskClass[] : []
-    
+
         const mergeTasksClass = [...parseNormalTasksData.map(t => ({ ...t, isOpened: false })), ...parseProjectsData.map(t => ({ ...t, isOpened: false }))];
         return mergeTasksClass.length == 0 ? null : mergeTasksClass;
     }
 
     return {
         saveDataToLocalStorage,
-        getDataFromLocalStorage 
+        getDataFromLocalStorage
     }
 }
